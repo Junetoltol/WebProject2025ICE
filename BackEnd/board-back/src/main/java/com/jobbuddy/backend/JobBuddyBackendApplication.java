e5bc764cf130d141bb4294fbf170c6aa9995c000
yunheedo <github1356@gmail.com>
2025-11-10 09:37:22 +0900

    백엔드 설정 파일 수정

---
+++ BackEnd/board-back/src/main/java/com/jobbuddy/backend/JobBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobbuddy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBuddyBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/board-back/src/test/java/com/jobbuddy/backend/JobBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobbuddy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
