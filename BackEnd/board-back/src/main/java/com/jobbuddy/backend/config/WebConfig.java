a877104661ee66ff698f4d895b64213478d8fa5c
yunheedo <github1356@gmail.com>
2025-11-27 11:04:34 +0900

    Update WebConfig.java
    프론트 테스트 주소 수정

--- BackEnd/board-back/src/main/java/com/jobbuddy/backend/config/WebConfig.java
+++ BackEnd/board-back/src/main/java/com/jobbuddy/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") //프론트 주소 허용(개발용)
+                .allowedOrigins("http://10.10.7.10:5173") //프론트 주소 허용(개발용)
                 .allowedMethods("*") //HTTP 메서드 전체 허용
                 .allowedHeaders("*") //모든 헤더 허용
                 .allowCredentials(true) //쿠키, Authorization 헤더 허용
