2ef750ae9fe41f2e062d1dcc6ef18e1c7e0a9dfe
yunheedo <github1356@gmail.com>
2025-11-27 10:09:13 +0900

    WebConfig.java에 주석 추가.
    WebConfig.java에 주석 추가.

--- BackEnd/board-back/src/main/java/com/jobbuddy/backend/config/WebConfig.java
+++ BackEnd/board-back/src/main/java/com/jobbuddy/backend/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000") //프론트 주소 허용(개발용)
+                .allowedMethods("*") //HTTP 메서드 전체 허용
+                .allowedHeaders("*") //모든 헤더 허용
+                .allowCredentials(true) //쿠키, Authorization 헤더 허용
                 .maxAge(3600);
     }
 }
